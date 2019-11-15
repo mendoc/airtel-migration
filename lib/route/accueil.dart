3fa4b6f4099969a5f1572f5182e6c1935e75f84d
mendoc <ongouadimitri5@gmail.com>
2019-11-15 08:43:38 +0100

    Restauration des contacts

--- lib/route/accueil.dart
+++ lib/route/accueil.dart
@@ -26,16 +26,14 @@ class AccueilPage extends StatefulWidget {
   _AccueilPageState createState() => _AccueilPageState();
 }
 
-void _showDialog(BuildContext context) {
+void _showDialog(BuildContext context, {String message}) {
   showDialog(
     context: context,
     builder: (BuildContext context) {
-      // return object of type Dialog
       return AlertDialog(
         title: new Text("Succès"),
-        content: new Text("Vos contacts ont bien été mis à jour."),
+        content: new Text(message ?? "Vos contacts ont bien été mis à jour."),
         actions: <Widget>[
-          // usually buttons at the bottom of the dialog
           new FlatButton(
             child: new Text(
               "Fermer",
@@ -60,6 +58,15 @@ class _AccueilPageState extends State<AccueilPage> {
     PermissionStatus permissionStatus = await _getContactPermission();
     if (permissionStatus == PermissionStatus.granted) {
       _getNumbers();
+    } else {
+      _handleInvalidPermissions(permissionStatus);
+    }
+  }
+
+  backupContacts() async {
+    PermissionStatus permissionStatus = await _getContactPermission();
+    if (permissionStatus == PermissionStatus.granted) {
+      _restoreNumbers();
     } else {
       _handleInvalidPermissions(permissionStatus);
     }
@@ -176,7 +183,8 @@ class _AccueilPageState extends State<AccueilPage> {
       }
       if (updated) {
         for (Item p in contact.phones ?? []) {
-          if (!nums.contains({"label": p.label, "value": p.value})) nums.add({"label": p.label, "value": p.value});
+          if (!nums.contains({"label": p.label, "value": p.value}))
+            nums.add({"label": p.label, "value": p.value});
         }
         m["phones"] = nums;
         print(nums);
@@ -191,6 +199,108 @@ class _AccueilPageState extends State<AccueilPage> {
       ready = true;
     });
     _showDialog(this.context);
+  }
+
+  Future _restoreNumbers() async {
+    List<String> numeros = [];
+    Iterable<Contact> contacts = await ContactsService.getContacts(
+        withThumbnails: false, photoHighResolution: false);
+    contacts = contacts.take(50);
+    int n = 1;
+
+    contacts.forEach((contact) {
+      Map m = contact.toMap();
+      List<Map> nums = [];
+      bool updated = false;
+      Iterable<Item> phones = contact.phones;
+      numeros.clear();
+      phones.forEach((phone) {
+        String p = phone.value.replaceAll(" ", "");
+        if (isGaboneseNumber(p)) {
+          Map it = {"label": phone.label, "value": phone.value};
+          nums.add(it);
+        } else {
+          if (isNewFormat(p)) updated = true;
+          print("Autre : " + p);
+        }
+      });
+
+      if (nums.length > 0 && updated) {
+        m["phones"] = nums;
+        print(m["displayName"]);
+        print(nums);
+        _createContact(Contact.fromMap(m));
+      }
+
+      setState(() {
+        progress = ((n / contacts.length) * 100).toString() + "%";
+      });
+      n++;
+    });
+    setState(() {
+      ready = true;
+    });
+    _showDialog(this.context);
+  }
+
+  bool isGaboneseNumber(String num) {
+    num = num.replaceAll(" ", "");
+    int tailleCour = num.length;
+
+    switch (tailleCour) {
+      case 13:
+        {
+          if (num.startsWith("002410")) {
+            num = num.substring(5);
+          }
+        }
+        break;
+      case 12:
+        {
+          if (num.startsWith("+2410")) {
+            num = num.substring(4);
+          }
+        }
+        break;
+      case 11:
+        {
+          if (num.startsWith("2410")) {
+            num = num.substring(3);
+          }
+        }
+        break;
+    }
+
+    return num.length == 8 && num.startsWith("0") && isNumeric(num);
+  }
+
+  bool isNewFormat(String num) {
+    num = num.replaceAll(" ", "");
+    int tailleCour = num.length;
+    bool inter = false;
+
+    switch (tailleCour) {
+      case 13:
+        {
+          if (num.startsWith("00241")) {
+            num = num.substring(5);
+            inter = true;
+          }
+        }
+        break;
+      case 12:
+        {
+          if (num.startsWith("+241")) {
+            num = num.substring(4);
+            inter = true;
+          }
+        }
+        break;
+    }
+    
+    if (inter) num = "0" + num;
+
+    return num.length == 9 && num.startsWith("0") && isNumeric(num) && (num.startsWith("07") || num.startsWith("06"));
   }
 
   Future<PermissionStatus> _getContactPermission() async {
@@ -250,7 +360,6 @@ class _AccueilPageState extends State<AccueilPage> {
                                 setState(() {
                                   progress = "0%";
                                   ready = false;
-                                  finished = false;
                                   refreshContacts();
                                 });
                               },
@@ -279,21 +388,30 @@ class _AccueilPageState extends State<AccueilPage> {
                             Padding(
                               padding:
                                   const EdgeInsets.symmetric(vertical: 30.0),
-                              child: Container(
-                                width: double.infinity,
-                                decoration: BoxDecoration(
-                                  color: Colors.red,
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      vertical: 20.0, horizontal: 5.0),
-                                  child: Center(
-                                    child: Text(
-                                      "Restaurer mes contacts",
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 18.0,
-                                          fontWeight: FontWeight.bold),
+                              child: InkWell(
+                                onTap: () {
+                                  setState(() {
+                                    progress = "0%";
+                                    ready = false;
+                                    backupContacts();
+                                  });
+                                },
+                                child: Container(
+                                  width: double.infinity,
+                                  decoration: BoxDecoration(
+                                    color: Colors.red,
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 20.0, horizontal: 5.0),
+                                    child: Center(
+                                      child: Text(
+                                        "Restaurer mes contacts",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 18.0,
+                                            fontWeight: FontWeight.bold),
+                                      ),
                                     ),
                                   ),
                                 ),
